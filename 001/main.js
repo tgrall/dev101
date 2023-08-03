9cbe3f1ebacc1ae8c07c455a6c2913ae453e39f6
tgrall <tugdual@gmail.com>
2023-08-03 10:30:43 +0200

    initial commit

---
+++ 001/main.js
@@ -0,0 +1,36 @@
+const menu = [
+    {
+        id: 1,
+        title: "Crepe au Sucre",
+        price: 2.50,
+        color : "beige",
+        
+    },
+    {
+        id: 2,
+        title: "Crepe au Chocolat",
+        price: 3.50,
+        color : "lightblue",
+    },    
+    {
+        id: 3,
+        title: "Crepe au Nutella",
+        price: 4.50,
+        color : "lightpink",
+    },
+git init];
+
+
+
+
+/**
+ *  Use this function to print the menu
+ */
+function load() {
+
+
+}
+
+// Call the function to exucute our business logic
+load();
+
